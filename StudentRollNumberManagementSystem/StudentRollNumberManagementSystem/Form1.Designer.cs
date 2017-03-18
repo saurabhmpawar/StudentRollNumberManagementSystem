5e7731bd2dd731954f325afe7472b3e94d5a5d7b
pavan_pawar <pavanpawar4591@gmail.com>
2017-03-18 07:26:24 +0530

    Initial Commit

---
+++ StudentRollNumberManagementSystem/StudentRollNumberManagementSystem/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace StudentRollNumberManagementSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.text_username = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblUderName = new System.Windows.Forms.Label();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textPassword
+            // 
+            this.textPassword.Location = new System.Drawing.Point(149, 62);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.PasswordChar = '*';
+            this.textPassword.Size = new System.Drawing.Size(100, 20);
+            this.textPassword.TabIndex = 11;
+            // 
+            // text_username
+            // 
+            this.text_username.Location = new System.Drawing.Point(149, 12);
+            this.text_username.Name = "text_username";
+            this.text_username.Size = new System.Drawing.Size(100, 20);
+            this.text_username.TabIndex = 10;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(48, 65);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.TabIndex = 9;
+            this.lblPassword.Text = "Password";
+            this.lblPassword.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblUderName
+            // 
+            this.lblUderName.AutoSize = true;
+            this.lblUderName.Location = new System.Drawing.Point(48, 19);
+            this.lblUderName.Name = "lblUderName";
+            this.lblUderName.Size = new System.Drawing.Size(60, 13);
+            this.lblUderName.TabIndex = 8;
+            this.lblUderName.Text = "User Name";
+            // 
+            // btn_close
+            // 
+            this.btn_close.Location = new System.Drawing.Point(152, 104);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(75, 23);
+            this.btn_close.TabIndex = 7;
+            this.btn_close.Text = "Close";
+            this.btn_close.UseVisualStyleBackColor = true;
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(51, 105);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(75, 23);
+            this.btn_login.TabIndex = 6;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 146);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.text_username);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUderName);
+            this.Controls.Add(this.btn_close);
+            this.Controls.Add(this.btn_login);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textPassword;
+        private System.Windows.Forms.TextBox text_username;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblUderName;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.Button btn_login;
+    }
+}
+
